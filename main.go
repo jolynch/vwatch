1b5b295f854fe835348739d9eb8042254ebd0fc0
Joseph Lynch <joe.e.lynch@gmail.com>
2025-03-09 00:31:50 -0500

    Switch to structured logging and implement jitter

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
+	"io"
+	"log/slog"
+	"math/rand"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
 
 var (
 	listen    = "127.0.0.1:8080"
+	blockFor  = 10 * time.Second
+	jitterFor = 1 * time.Second
+	logLevel  = new(slog.LevelVar)
 	versions  sync.Map
 	watchers  map[string]Watcher = make(map[string]Watcher)
 	watchLock sync.Mutex
@@ -54,7 +60,7 @@ func storeNewVersion(name string, version Version) {
 
 func version(w http.ResponseWriter, req *http.Request) {
 	var (
-		timeout time.Duration = time.Duration(10 * time.Second)
+		timeout time.Duration = blockFor
 		err     error
 		version Version
 		name    string = req.PathValue("name")
@@ -83,7 +89,7 @@ func version(w http.ResponseWriter, req *http.Request) {
 		if ok && len(t[0]) >= 0 {
 			timeout, err = time.ParseDuration(t[0])
 			if err != nil {
-				http.Error(w, "Invalid timeout, try something like 10s", http.StatusBadRequest)
+				http.Error(w, "Invalid timeout duration, try something like 10s", http.StatusBadRequest)
 				return
 			}
 		}
@@ -109,9 +115,9 @@ func version(w http.ResponseWriter, req *http.Request) {
 
 				select {
 				case <-watcher.Signal:
-					log.Printf("Unblocking due to signal")
+					slog.Debug(fmt.Sprintf("Unblocking GET[%s] due to signal", name))
 				case <-time.After(timeout):
-					log.Printf("Unblocking due to %s timeout", timeout)
+					slog.Debug(fmt.Sprintf("Unblocking GET[%s] due to %s timeout", name, timeout))
 				}
 				watcher.WatchGroup.Done()
 
@@ -120,6 +126,11 @@ func version(w http.ResponseWriter, req *http.Request) {
 					http.Error(w, fmt.Sprintf("%s not found", name), http.StatusNotFound)
 				}
 				version = val.(Version)
+				if jitterFor.Milliseconds() > 0 {
+					jitterDuration := rand.Int63n(jitterFor.Milliseconds())
+					w.Header().Set("Jittered", fmt.Sprintf("%d ms", jitterDuration))
+					time.Sleep(time.Duration(jitterDuration) * time.Millisecond)
+				}
 			}
 		}
 	default:
@@ -127,25 +138,49 @@ func version(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	w.Header().Set("Version", version.Version)
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(version)
-	if err != nil {
-		log.Printf("ERROR %s", err)
+	w.Header().Set("ETag", version.Version)
+	w.Header().Set("Last-Modified", version.LastSync.UTC().Format(http.TimeFormat))
+	io.WriteString(w, version.Version)
+}
+
+func setLogLevel(w http.ResponseWriter, req *http.Request) {
+	level, ok := req.URL.Query()["level"]
+	if ok {
+		slevel := slog.Level(0)
+		err := slevel.UnmarshalText([]byte(level[0]))
+		if err != nil {
+			return
+		}
+		slog.SetLogLoggerLevel(slevel)
+		io.WriteString(w, logLevel.Level().String())
+	} else {
+		http.Error(w, "/log requires a ?level=DEBUG param", http.StatusBadRequest)
 	}
 }
 
+var paths = `Paths
+GET /version/{repository}[:{tag}]?[version=last_seen]    -> Get latest version or block for new version
+PUT /version/{repository}[:{tag}] {"version": <version>} -> Set latest version, unblocking watches
+PUT /log?level=DEBUG                                     -> Set log level
+`
+
 func main() {
-	flag.StringVar(&listen, "listen", listen, "The address to listen on, defaults to 127.0.0.1:8080")
+	flag.StringVar(&listen, "listen", listen, "The address to listen on")
+	flag.DurationVar(&blockFor, "block-for", blockFor, "The duration to block GETs by default for")
+	flag.DurationVar(&jitterFor, "jitter-for", jitterFor, "The duration to jitter blocking GETs by, should be less than block-for")
 	flag.Parse()
 
-	http.HandleFunc("/version/{name}", version)
+	http.HandleFunc("/version/{name...}", version)
+	http.HandleFunc("PUT /log", setLogLevel)
 
-	log.Printf("Listening at %s/version", listen)
+	slog.Info(fmt.Sprintf("Listening at %s", listen))
+
+	slog.Info(paths)
 	err := http.ListenAndServe(listen, nil)
 	if err != nil {
-		log.Fatalf("Failed to bind, is another server listening at this address?")
+		slog.Error("Failed to bind, is another server listening at this address?")
+		os.Exit(1)
 	} else {
-		log.Printf("Success!")
+		slog.Info("All Done!")
 	}
 }
